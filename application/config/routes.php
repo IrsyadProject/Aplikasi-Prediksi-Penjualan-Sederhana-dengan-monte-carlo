c9ce74bf3077a2295bcb4c28221bcf4ad3da3c5a
IrsyadProject <39052384+IrsyadProject@users.noreply.github.com>
2019-11-15 09:04:45 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'Front_Controller';
 $route['about'] = 'Front_Controller/tentang';
+$route['hitung'] = 'Front_Controller/hitung';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
